6aaf97529529bc26df3341c563b2866ec976a1b8
moranxiao <yyyjka@foxmail.com>
2024-10-14 17:14:44 +0800

    模拟实现list

--- List/list.hpp
+++ List/list.hpp
@@ -1,13 +1,13 @@
 #pragma once
 #pragma warning(disable:4996)
 #include <utility>
-
+#include <iostream>
 
 namespace moran {
 	template <class T>
-	struct ListNode {
-		typedef ListNode<T> Node;
-		List_Node(const T& value, Node* prev = nullptr, Node* next = nullptr)
+	struct list_node {
+		typedef list_node<T> Node;
+		list_node(const T& value = T(), Node* prev = nullptr, Node* next = nullptr)
 			:_value(value),
 			_prev(prev),
 			_next(next)
@@ -17,36 +17,249 @@ namespace moran {
 		Node* _next;
 	};
 	
+	template<class T,class Ref,class Ptr>
+	struct list_iterator {
+		typedef list_iterator<T,Ref,Ptr> Self;
+		typedef list_node<T> Node;
+
+		list_iterator(Node* node)
+			:_node(node)
+		{}
+		Self& operator++()
+		{
+			_node = _node->_next;
+			return *this;
+		}
+		//不能用引用，tmp是临时变量
+		Self operator++(int)
+		{
+			Self tmp(_node);  
+			_node = _node->_next;
+			return tmp;
+		}
+		Self& operator--()
+		{
+			_node = _node->_prev;
+			return *this;
+		}
+		Self operator--(int)
+		{
+			Self tmp(_node);
+			_node = _node->_prev;
+			return tmp;
+		}
+		bool operator!=(const Self& it) const
+		{
+			return it._node != _node;
+		}
+		bool operator==(const Self& it) const
+		{
+			return it._node == _node;
+
+		}
+		Ref operator*() const 
+		{
+			return _node->_value;
+		}
+		Ptr operator->() const
+		{
+			return &(operator*());
+		}
+
+		Node* _node;
+	};
+
 	template <class T>
 	class list {
-		typedef ListNode<T> Node;
+		typedef list_node<T> Node;
+	//共有成员类型
 	public:
-			
-	private:
-		Node* create_node(const T& value)
+		typedef list_iterator<T, T&, T*> iterator;
+		typedef list_iterator<T, const T&, const T*> const_iterator;
+
+		iterator begin()
 		{
-			Node* new_node = new Node(value);
-			return new_node;
+			return iterator(_head->_next);
+		}
+		iterator end()
+		{
+			return iterator(_head);
+		}
+		const_iterator cbegin() const
+		{
+			return const_iterator(_head->_next);
+		}
+		const_iterator cend() const
+		{
+			return const_iterator(_head);
+		}
+		void print()
+		{
+			for (auto& e : *this)
+			{
+				std::cout << e << " ";
+			}
+			std::cout << std::endl;
+		}
+	//私有成员方法
+	private:
+		void init()
+		{
+			_head = new Node();
+			_head->_next = _head->_prev = _head;
 		}
 	public:
 		list()
 		{
-			_head = create_node(0);
+			init();
 		}
+
 		list(size_t n, const T& value = T())
 		{
+			//选择头插尾插一样的，我选择尾插
+			init();
+			_size = n;
 			while (n--)
 			{
-				Node* new_node = create_node(value);
-				
+				Node* new_node = new Node(value, _head->_prev, _head);
+				_head->_prev->_next = new_node;
+				_head->_prev = new_node;
 			}
-			
 		}
 		list(int n, const T& value = T())
 		{
+			init();
+			_size = n;
+			while (n--)
+			{
+				Node* new_node = new Node(value, _head->_prev, _head);
+				_head->_prev->_next = new_node;
+				_head->_prev = new_node;
+			}
+		}
+
+		template<class InputIterator>
+		list(InputIterator first, InputIterator last)
+		{
+			init();
+			while (first != last)
+			{
+				Node* new_node = new Node(*first,_head->_prev,_head);
+				_head->_prev->_next = new_node;
+				_head->_prev = new_node;
+				first++;
+				_size++;
+			}
+		}
+		//列表初始化，非常的方便！
+		list(std::initializer_list<T> initializer)
+		{
+			init();
+			for (auto& e : initializer)
+			{
+				Node* new_node = new Node(e, _head->_prev, _head);
+				_head->_prev->_next = new_node;
+				_head->_prev = new_node;
+				_size++;
+			}
+		}
+
+		void swap(list& lt)
+		{
+			std::swap(lt._head, _head);
+			std::swap(lt._size, _size);
+		}
+		list(const list& lt)
+		{
+			init();
+			for (auto& e : lt)
+			{
+				push_back(e);
+			}
+		}
+		list& operator=(const list lt)
+		{
+			swap(lt, *this);
+			return *this;
+		}
+		//增
+		void push_back(const T& value)
+		{
+			Node* new_node = new Node(value, _head->_prev, _head);
+			_head->_prev->_next = new_node;
+			_head->_prev = new_node;
+			_size++;
+		}
+		void push_front(const T& value)
+		{
+			Node* new_node = new Node(value, _head, _head->_next);
+			_head->_next = new_node;
+			new_node->_next->_prev = new_node;
+			_size++;
 
 		}
+
+		//在it之后插入
+		iterator insert(iterator it,const T& value)
+		{
+			Node* new_node = new Node(value, it._node, it._node->_next);
+			it._node->_next = new_node;
+			new_node->_next->_prev = new_node;
+			_size++;
+			return it;
+		}
+		//删
+		void pop_front()
+		{
+			if (_size == 0)
+				return;
+			_head->_next = _head->_next->_next;
+			delete _head->_next->_prev;
+			_head->_next->_prev = _head;
+			_size--;
+		}
+		void pop_back()
+		{
+			if (_size == 0)
+				return;
+			_head->_prev = _head->_prev->_prev;
+			delete _head->_prev->_next;
+			_head->_prev->_next = _head;
+			_size--;
+		}
+		iterator erase(iterator it)
+		{
+			Node* tmp = it._node->_next;
+			it._node->_prev->_next = tmp;
+			tmp->_prev = it._node->_prev;
+			delete it._node;
+			return iterator(tmp);
+		}
+
+		~list()
+		{
+			Node* cur = _head->_next;
+			while (cur != _head)
+			{
+				Node* tmp = cur->_next;
+				delete cur;
+				cur = tmp;
+			}
+			delete _head;
+		}
+		size_t size()
+		{
+			return _size;
+		}
+
+		bool empty()
+		{
+			return _size == 0;
+		}
+
+
 	private:
-		Node* _head;
+		Node* _head = nullptr;
+		size_t _size = 0;
 	};
 }
---
+++ List/test.cpp
@@ -0,0 +1,53 @@
+#include "list.hpp"
+#include <list>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using namespace moran;
+
+int main()
+{
+	//list<int> l1(10,5);
+	//l1.print();
+	//list<double> l2(10,5.5);
+	//l2.print();
+	//int array[] = { 1,2,3,4,6,7,8,9,10 };
+	//list<int> l3(array, array + sizeof(array) / sizeof(int));
+	//l3.print();
+	//list<int> lt4({ 1,2,3,4,5,6,7,8,9 });
+	//lt4.print();
+	//list<int> lt5 = { 1,2,3,4,5,6,7,8,9 };
+	//lt5.print();
+	//list<int> lt6 = { 1,6,9,11,38,77 };
+	//lt6.print();
+	//lt6.push_front(100);
+	//lt6.push_back(200);
+	//lt6.print();
+	//lt6.pop_back();
+	//lt6.pop_back();
+	//lt6.pop_back();
+	//lt6.pop_back();
+	//lt6.print();
+	//lt6.pop_front();
+	//lt6.pop_front();
+	//lt6.print();
+	//lt6.pop_front();
+	//lt6.pop_front();
+	//lt6.pop_front();
+	//lt6.pop_front();
+	//lt6.print();
+	//list<int> lt7({ 1,2,3,4,5,6,7,8,9 });
+	//lt7.print();
+	//auto it = lt7.begin();
+	//it++;
+	//it--;
+	//++it;
+	//--it;
+	//lt7.insert(it, 100);
+	//lt7.erase(it);
+	//lt7.print();
+	//lt7.erase(it);
+
+	return 0;
+}
